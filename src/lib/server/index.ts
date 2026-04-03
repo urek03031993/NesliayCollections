f30ee28bc7a2bf6fafd2933343d89fa7fe2b4aaf
asd331993 <ofeliaq020463@gmail.com>
2026-04-03 09:50:20 -0400

    Update

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -12,15 +12,21 @@ import * as schema from './db/index';
 
 
 // Production environment variables
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
-import { config } from "dotenv";
+// import { drizzle } from "drizzle-orm/neon-http";
+// import { neon } from "@neondatabase/serverless";
+// import { config } from "dotenv";
 
-config({ path: ".env" }); // or .env.local
+// config({ path: ".env" });
 
-const sql = neon(process.env.DATABASE_URL!);
+// const sql = neon(process.env.DATABASE_URL!);
 
-export const db = drizzle(sql, { schema });
+// export const db = drizzle(sql, { schema });
 
+
+import { Pool } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-serverless';
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const db = drizzle(pool, { schema });
 
 
