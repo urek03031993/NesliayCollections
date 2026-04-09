61decd2900098489c920cc7536dc99df5ffaebd8
asd331993 <ofeliaq020463@gmail.com>
2026-04-08 20:15:46 -0400

    update

--- src/lib/components/forms/StripePaymentForm/StripePaymentsForm.svelte
+++ src/lib/components/forms/StripePaymentForm/StripePaymentsForm.svelte
@@ -1,85 +1,86 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { loadStripe } from '@stripe/stripe-js';
-	import type { Stripe, StripeElements } from '@stripe/stripe-js';
-	import { PUBLIC_STRIPE_TEST_PUBLISHABLE_KEY } from '$env/static/public';
-	import { Elements, LinkAuthenticationElement, PaymentElement, Address } from 'svelte-stripe';
-	import { cart, cartTotal } from '$lib/stores/store';
+	// import { onMount } from 'svelte';
+	// import { loadStripe } from '@stripe/stripe-js';
+	// import type { Stripe, StripeElements } from '@stripe/stripe-js';
+	// import { PUBLIC_STRIPE_TEST_PUBLISHABLE_KEY } from '$env/static/public';
+	// import { Elements, LinkAuthenticationElement, PaymentElement, Address } from 'svelte-stripe';
+	// import { cart, cartTotal } from '$lib/stores/store';
 
 	let { startDate, endDate } = $props(); 
 
-	let stripe = $state<Stripe | null>();
-	let clientSecret = $state<string | null>(null);
-	let error = $state<string | null>();
-	let elements = $state<StripeElements>();
-	let processing = $state(false);
+	// let stripe = $state<Stripe | null>();
+	// let clientSecret = $state<string | null>(null);
+	// let error = $state<string | null>();
+	// let elements = $state<StripeElements>();
+	// let processing = $state(false);
 
-	onMount(async () => {
-		stripe = await loadStripe(PUBLIC_STRIPE_TEST_PUBLISHABLE_KEY);
-		clientSecret = await createPaymentIntent();
-	});
+	// onMount(async () => {
+	// 	stripe = await loadStripe(PUBLIC_STRIPE_TEST_PUBLISHABLE_KEY);
+	// 	clientSecret = await createPaymentIntent();
+	// });
 	
-	async function createPaymentIntent() {
-		const response = await fetch('/api/stripe/create-payment-intent', {
-			method: 'POST',
-			headers: {
-				'content-type': 'application/json'
-			},
-			body: JSON.stringify({ total: $cartTotal })
-		});
+	// async function createPaymentIntent() {
+	// 	const response = await fetch('/api/stripe/create-payment-intent', {
+	// 		method: 'POST',
+	// 		headers: {
+	// 			'content-type': 'application/json'
+	// 		},
+	// 		body: JSON.stringify({ total: $cartTotal })
+	// 	});
 
-		const { clientSecret } = await response.json();
+	// 	const { clientSecret } = await response.json();
 
-		return clientSecret;
-	}
-
-
-	async function submit(event: SubmitEvent) {
-		event.preventDefault();	
-
-		if (processing || !stripe || !elements || !clientSecret) return;
-
-		processing = true;
-
-		await elements.submit();
-
-		const result = await stripe.confirmPayment({
-			elements,
-			clientSecret,
-			confirmParams: {
-				return_url: `${window.location.origin}/checkout/payment/thanks`
-			}
-		});
+	// 	return clientSecret;
+	// }
 
 
+	// async function submit(event: SubmitEvent) {
+	// 	event.preventDefault();	
 
-		if (result.error) {
-			error = result.error.message;
-			processing = false;
+	// 	if (processing || !stripe || !elements || !clientSecret) return;
 
-		}else{
+	// 	processing = true;
 
-			const response = await fetch('/api/checkout', {
-				method: 'POST',
-				headers: {
-					'content-type': 'application/json'
-				},
-				body: JSON.stringify({
-					startDate:  startDate, 
-					endDate: endDate,
-					total: $cartTotal,
-					tax: Math.round($cartTotal * 100) / 100,
-					orderItems: $cart					
-				})
-			});
-			const jsonCheckout = await response.json();
+	// 	await elements.submit();
 
-			processing = false;
-			console.log(jsonCheckout)
-		}
-	}
+	// 	const result = await stripe.confirmPayment({
+	// 		elements,
+	// 		clientSecret,
+	// 		confirmParams: {
+	// 			return_url: `${window.location.origin}/checkout/payment/thanks`
+	// 		}
+	// 	});
+
+
+
+	// 	if (result.error) {
+	// 		error = result.error.message;
+	// 		processing = false;
+
+	// 	}else{
+
+	// 		const response = await fetch('/api/checkout', {
+	// 			method: 'POST',
+	// 			headers: {
+	// 				'content-type': 'application/json'
+	// 			},
+	// 			body: JSON.stringify({
+	// 				startDate:  startDate, 
+	// 				endDate: endDate,
+	// 				total: $cartTotal,
+	// 				tax: Math.round($cartTotal * 100) / 100,
+	// 				orderItems: $cart					
+	// 			})
+	// 		});
+	// 		const jsonCheckout = await response.json();
+
+	// 		processing = false;
+	// 		console.log(jsonCheckout)
+	// 	}
+	// }
 </script>
-
+<p>{startDate} to {endDate}</p>
+<!-- 
 <h4 class="font-notoSerif text-primary mb-4 text-5xl tracking-tight md:text-6xl">
 	Order Payment
 </h4>
@@ -110,7 +111,7 @@
     Loading...
 {/if}
 
-
+ -->
 <style>
 	.error {
 		color: tomato;
